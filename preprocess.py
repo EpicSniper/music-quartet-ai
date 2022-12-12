7a9a60a3d53afe6bbe4c875acb8c5fd3ec576834
Epic_Sniper <smith.lukas@seznam.cz>
2022-12-12 05:12:55 -0800

    Kodovani mid souboru na string a ukladani stringu do souboru

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@ import os
 import music21 as m21
 
 MIDI_DATASET_PATH = "MIDI/test"
+SAVE_DIR = "dataset"
 
 # durations are expressed in quarter length
 ACCEPTABLE_DURATIONS = [
@@ -39,7 +40,10 @@ def transpose(piece):
     # vycist predznamenani
     parts = piece.getElementsByClass(m21.stream.Part)
     measures_part0 = parts[0].getElementsByClass(m21.stream.Measure)
-    key = measures_part0[0][4]
+    if len(measures_part0[0]) >= 5:
+        key = measures_part0[0][4]
+    else:
+        key = ""
 
     # odhadnout predznamenani
     if not isinstance(key, m21.key.Key):
@@ -63,7 +67,7 @@ def preprocess (dataseyt_path):
     songs = load_pieces_in_midi(dataseyt_path)
     print(f"Loaded {len(pieces)} pieces.")
 
-    for piece in pieces:
+    for i, piece in enumerate(pieces):
 
         # vyfiltrovat skladby, kde jsou trioly, neni 4/4 atd.
         if not has_acceptable_durations(piece, ACCEPTABLE_DURATIONS):
@@ -73,8 +77,49 @@ def preprocess (dataseyt_path):
         piece = transpose(piece)
 
         # zakodovat skladby do formatu, kteremu bude neuronka rozumnet
+        encoded_piece = encode_piece(piece)
 
-        # ulozit data skladeb do txt souboru
+        # ulozit data skladeb do souboru
+        save_path = os.path.join(SAVE_DIR, str(i))
+        with open(save_path, "w") as fp:
+            fp.write(encoded_piece)
+
+def encode_piece(piece, time_step=0.25):
+    # pitch = 60, duration = 1 -> [60, "_", "_", "_"]
+    # TODO: pro vice stop
+
+    encoded_piece = []
+
+    for event in piece.flat.notesAndRests:
+
+        # zpracovani not
+        if isinstance(event, m21.note.Note):
+            symbol = event.pitch.midi
+
+        # zpracovani pomlk
+
+        if isinstance(event, m21.note.Rest):
+            symbol = "r"
+        
+        if isinstance(event, m21.chord.Chord):
+            # TODO: implementovat dvojhmaty/akordy
+            # z dvojhmatu/akordu vybere ten nejnizsi ton
+            symbol = event[0].pitch.midi
+        
+        # konvertace noty a pomlky
+        # TODO: pokud budu chtit trioly (triola), tak zde zmenit time_step na 1/12 a event.duration na kratsi
+        steps = int (event.duration.quarterLength / time_step)
+        for step in range(steps):
+            if step == 0:
+                encoded_piece.append(symbol)
+            else:
+                encoded_piece.append("_")
+    
+    # zpracovani do stringu
+    encoded_piece = " ".join(map(str, encoded_piece))
+
+    return encoded_piece
+
 
 if __name__ == "__main__":
 
@@ -82,8 +127,7 @@ if __name__ == "__main__":
     print(f"Loaded {len(pieces)} pieces.")
     piece = pieces[1]
 
-    print(f"Has acceptable duration? {has_acceptable_durations(piece, ACCEPTABLE_DURATIONS)}")
+    preprocess(MIDI_DATASET_PATH)
 
     transposed_piece = transpose(piece)
-
-    transposed_piece.show()
+    #transposed_piece.show()
