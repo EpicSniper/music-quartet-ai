93e99f98f761c353f81e60f0295bdb36750dc7ec
Epic_Sniper <smith.lukas@seznam.cz>
2022-12-13 03:29:15 -0800

    Cloudsave

--- test1.py
+++ test1.py
@@ -1,14 +1,5 @@
 from preprocess import load_pieces_in_midi
 import music21 as m21
+import tensorflow as tf
 
-piece = load_pieces_in_midi("MIDI/test1")
-print(piece[0])
-parts = []
-
-for part in piece[0]:
-    print(part)
-    if isinstance(part, m21.stream.Part):
-        for measure in part:
-            print(measure)
-            for event in measure:
-                print(event)
+print(tf.config.list_physical_devices('GPU'))
